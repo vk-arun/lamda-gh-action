ae75fe0da7ee5ff08fb0607f133100183bc91537
arun <vk97arun@gmail.com>
2022-04-08 10:00:21 +0530

    handler default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.generateRandomNumber = async (event) => {
+module.exports.handler = async (event) => {
 const randomNumber = parseInt(Math.random()*100);
 console.log('first lambda ');
 const response = {
