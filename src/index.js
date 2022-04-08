d10eb3cdd33e6af60e5db5e68a14cf4c80cf01a0
arun <vk97arun@gmail.com>
2022-04-08 10:59:59 +0530

    handler default

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const randomNumber = parseInt(Math.random()*100);
 console.log('first lambda ');
 const response = {
     statusCode: 200,
-    body: JSON.stringify(`This is random number is1 = ${randomNumber}`),
+    body: JSON.stringify(`This is random number is2 = ${randomNumber}`),
 };
 return response;
 };
